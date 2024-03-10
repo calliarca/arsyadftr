e2eb73900d739e6ffeac54ee4b918759a6cd6620
calliarca <arsyaddod123@gmail.com>
2024-03-10 20:19:38 +0700

    favicon changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,20 +11,26 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Arsyad Faturrahman Website",
+  title: "Arsyad Portofolio",
   description: "Portofolio Website by Arsyad Faturrahman",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={outfit.className}>
         <ThemeProvider attribute="class" defaultTheme='light'>
-        <Header />
-        {children}
-        <Footer />
+          <Header />
+          {children}
+          <Footer />
         </ThemeProvider>
-        </body>
+      </body>
     </html>
   );
 }
